3c8b26c5d2c9fa446e06ec5e5020608c4fa6af2f
jiteshgupta <jiteshg@seas.upenn.edu>
2016-05-01 01:12:43 -0400

    Adding testing functions in StorageAPI

--- DLMS/src/edu/upenn/cis550/storage/StorageAPI.java
+++ DLMS/src/edu/upenn/cis550/storage/StorageAPI.java
@@ -1,9 +1,13 @@
 package edu.upenn.cis550.storage;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 import com.sleepycat.je.DatabaseException;
+import com.sleepycat.persist.EntityCursor;
 
 import edu.upenn.cis550.extractor.Struct;
 
@@ -58,6 +62,51 @@ public class StorageAPI {
 			}
 			da.wordByValue.put(index);
 		}
+	}
+	
+	public void showAllWords() throws DatabaseException{
+		EntityCursor<InvertedIndex> items = da.wordByValue.entities();
+		for(InvertedIndex item: items){
+			System.out.println("<------->");
+			String word = item.getWord();
+			System.out.println("\nWord:- " + word);
+			HashSet<Integer> nodeIDs = item.getGraphNodes();
+			Iterator<Integer> iter = nodeIDs.iterator();
+			while(iter.hasNext()){
+				System.out.print(iter.next());
+				System.out.print("\t");
+			}
+			System.out.println("");
+		}
+		items.close();
+	}
+	
+	public void showAllNodes() throws DatabaseException, IOException{
+		/** Get a cursor that will walk every
+		 *  node object in the store
+		 */
+		EntityCursor<GraphNode> items = da.nodeByID.entities();
+		
+		for(GraphNode item: items){
+			System.out.println("<------->");
+			System.out.println("\tNode id :"  + item.getNodeID());
+			System.out.println("\tDoc Id :"  + item.getDocumentID());
+			System.out.println("\tName :"  + item.getName());
+			System.out.println("\tType :"  + item.getType());
+			System.out.println("\tValue :"  + item.getValue());
+			System.out.println("\tParent :"  + item.getParent());
+			System.out.print("\tChildren : ");
+			
+			if(!(item.getChildren() == null)){
+				for (int j : item.getChildren()){
+					System.out.print(j + " " );
+				}
+				System.out.println("\t");
+			}else{
+				System.out.println("None");
+			}
+		}
+		items.close();
 	}
 	
 	public boolean checkName(String type){
