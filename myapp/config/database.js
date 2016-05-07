0dbe2f7dd659b44386aaf3c2d2508179085d26b0
manisharma <mani_sharma43@yahoo.com>
2016-05-07 00:48:17 -0400

    myapp

---
+++ myapp/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://mani:Dash468#@jello.modulusmongo.net:27017/ojyz7aPe' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
