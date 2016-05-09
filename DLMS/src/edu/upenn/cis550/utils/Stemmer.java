4766c2bf3bbdad5afd79723f442369f910a2dd3e
Sanidhya <sanidhya@seas.upenn.edu>
2016-05-09 12:29:32 -0400

    making stemmer public

--- DLMS/src/edu/upenn/cis550/utils/Stemmer.java
+++ DLMS/src/edu/upenn/cis550/utils/Stemmer.java
@@ -8,7 +8,7 @@ package edu.upenn.cis550.utils;
   * by calling one of the various stem(something) methods.
   */
 
-class Stemmer
+public class Stemmer
 {  private char[] b;
    private int i,     /* offset into b */
                i_end, /* offset to end of stemmed word */
