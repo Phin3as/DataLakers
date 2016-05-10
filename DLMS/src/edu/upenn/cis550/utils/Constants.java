8a518ad45cf37b9e9227278a1f334ee738406a77
jiteshgupta <jiteshg@seas.upenn.edu>
2016-05-09 21:16:51 -0400

    Editing Home Path

--- DLMS/src/edu/upenn/cis550/utils/Constants.java
+++ DLMS/src/edu/upenn/cis550/utils/Constants.java
@@ -2,15 +2,14 @@ package edu.upenn.cis550.utils;
 
 public class Constants {
 	
-	public final static String HOME = "C:\\Users\\Sanidhya";
-	public final static String PATH_DIR = HOME + "\\git\\DataLakers\\graph11";
-	public final static String PATH_DICT = HOME + "\\git\\DataLakers\\WordNet\\2.1\\dict";
-	public final static String PATH_SPELL = HOME + "\\git\\DataLakers\\spell.py";
-	public final static String PATH_FILES = HOME + "\\git\\DataLakers\\sampleXml";
+	public final static String HOME = "C:\\Users\\Sanidhya\\git\\";
+	public final static String PATH_DIR = HOME + "DataLakers\\graph11";
+	public final static String PATH_DICT = HOME + "DataLakers\\WordNet\\2.1\\dict";
+	public final static String PATH_SPELL = HOME + "DataLakers\\spell.py";
+	public final static String PATH_FILES = HOME + "DataLakers\\sampleXml";
 	public final static int MAX_RESULTS = 10;
 	public final static boolean IS_STEM = true;
 	public final static boolean IS_SYNONYM = true;
 	public final static boolean IS_SPELL_CHECK = true;	
 	public final static int DEPTH = 10;
-	
 }
